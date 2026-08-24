d1d5d363f5e6c4f461c654807df838bf6309bd42
Raziman T V <razimantv@gmail.com>
2026-08-24 22:54:49 +0100

    Trim line length to 79

--- fzf.py
+++ fzf.py
@@ -20,7 +20,10 @@ def print_error(message: str) -> None:
 def fzf_process_cb(
     data: str, command: str, return_code: int, out: str, err: str
 ) -> int:
-    if return_code == weechat.WEECHAT_HOOK_PROCESS_ERROR or return_code == 2 or err:
+    if (
+            return_code == weechat.WEECHAT_HOOK_PROCESS_ERROR
+            or return_code == 2 or err
+    ):
         print_error("Error running fzf (code {}): {}".format(return_code, err))
         return weechat.WEECHAT_RC_OK
     if out != "":
@@ -34,7 +37,9 @@ def fzf_command_cb(data: str, buffer: str, args: str) -> int:
         "fzf --popup -- --delimiter='\t' --with-nth=3.. "
         "--preview='tail -$LINES {2} 2>/dev/null'"
     )
-    hook = weechat.hook_process_hashtable(cmd, {"stdin": "1"}, 0, "fzf_process_cb", "")
+    hook = weechat.hook_process_hashtable(
+        cmd, {"stdin": "1"}, 0, "fzf_process_cb", ""
+    )
     for buffer_info in buffers():
         weechat.hook_set(hook, "stdin", "\t".join(buffer_info) + "\n")
     weechat.hook_set(hook, "stdin_close", "")
@@ -61,7 +66,8 @@ def buffers() -> Iterator[Tuple[str, str, str, str]]:
 
 def main() -> None:
     if not weechat.register(
-        SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""
+        SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
+        SCRIPT_DESC, "", ""
     ):
         return
 
@@ -74,7 +80,9 @@ def main() -> None:
         )
         return
 
-    weechat.hook_command(SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_command_cb", "")
+    weechat.hook_command(
+        SCRIPT_NAME, SCRIPT_DESC, "", "", "", "fzf_command_cb", ""
+    )
 
 
 if __name__ == "__main__":
